abc5852e7c006adb626eaac2bc885ac71c548d72
Miguel Figueroa <miguelfigueroa90@gmail.com>
2021-03-29 11:27:23 -0400

    custom admin user

--- users/admin.py
+++ users/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
 from users.models import Profile
 
 @admin.register(Profile)
@@ -15,3 +17,41 @@ class ProfileAdmin(admin.ModelAdmin):
         'user__last_name', 
         'phone_number'
     )
+
+    fieldsets = (
+        ('Profile', {
+            'fields': (('user', 'picture'),),
+        }),
+        ('Extra info', {
+            'fields': (
+                ('website', 'phone_number'),
+                ('biography')
+            )
+        }),
+        ('Metadata', {
+            'fields': (('created', 'modified'),)
+        })
+    )
+
+    readonly_fields = ('created', 'modified')
+
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    can_delete = False
+    verbose_name_plural = 'profiles'
+
+class UserAdmin(BaseUserAdmin):
+    inlines = (ProfileInline,)
+
+    list_display = (
+        'username',
+        'email',
+        'first_name',
+        'last_name',
+        'is_active',
+        'is_staff',
+    )
+
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
