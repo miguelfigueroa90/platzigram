ad786bcf0e2d349df24850e7a3fb8e62bc31eb38
Miguel Figueroa <miguelfigueroa90@gmail.com>
2021-03-29 12:08:27 -0400

    media

--- platzigram/urls.py
+++ platzigram/urls.py
@@ -1,4 +1,6 @@
 # Django
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 # Local
@@ -7,4 +9,4 @@ from posts import views as posts_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('posts/', posts_views.posts_list),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
